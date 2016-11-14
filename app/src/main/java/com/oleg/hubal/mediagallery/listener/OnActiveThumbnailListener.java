d2c6292116be6245aeee382cc0db635303d06ee7
hubaloleg <hubal.oleg@gmail.com>
2016-11-14 18:27:37 +0200

    video pause/play + seek video

--- app/src/main/java/com/oleg/hubal/mediagallery/listener/OnActiveThumbnailListener.java
+++ app/src/main/java/com/oleg/hubal/mediagallery/listener/OnActiveThumbnailListener.java
@@ -1,9 +1,10 @@
 package com.oleg.hubal.mediagallery.listener;
 
 /**
- * Created by User on 13.11.2016.
+ * Created by User on 14.11.2016.
  */
 
 public interface OnActiveThumbnailListener {
-    public void onActiveThumbnail(int positions);
+    void onActiveThumbnail(int position);
 }
+
